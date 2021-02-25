61954d24a06e9b30175e1d2a152188c8792c1133
Ruthesh Muniraj <rutheshmuniraj@outlook.com>
2021-02-25 20:19:29 +0530

    added 05enumeratedTypes

---
+++ 05enumeratedTypes.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+enum GENDER
+{
+    male,
+    female
+};      // <---- Check other way
+int main()
+{
+    GENDER gender = male;       // <----
+    switch (gender)
+    {
+    case male:
+        std::cout << "The gender is Male" << std::endl;
+        break;
+    case female:
+        std::cout << "The gender is Memale" << std::endl;
+        break;
+    }
+}
+
+
+/*
+//It works either way
+enum GENDER
+{
+    male,
+    female
+} g;        // <----
+int main()
+{
+    g = male;       // <----
+    switch (g)
+    {
+    case male:
+        std::cout << "The gender is Male" << std::endl;
+        break;
+    case female:
+        std::cout << "The gender is Memale" << std::endl;
+        break;
+    }
+}
+
+*/
